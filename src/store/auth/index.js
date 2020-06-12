785e4d79871ada5d1cb2980d2a53319002dd12ab
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-06-12 17:40:57 +0900

    Update index.js

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -6,7 +6,7 @@ const auth = {
   state: {
     accessToken: 'BQCJdU2y_nYNtx_JjDLURI3nQopmLLgo9ANKcJLO5FwT4W99bafV2ju-bbjRFVUQks0a856iVxIChDg8BWc5AyTtoRtqvZh7nUDLGpmJlWF3Xaw4tQZjeot3LBd4v7CLvF25rrxOatDg0S0mbQEUVjqSXn0nAAgQZmUN42FHs15_OME_wRHp-pI9aiIduvDYm9WBV05xpkmyRX0MTURJCHrvnOM3l7vjoyxXI13_NmsRpAl1-LANNXkzVUUtSi6gLhS6Mf6waX3_wwZ__952Gj4pVTGvHdRinf0',
     refreshToken: 'BQCJdU2y_nYNtx_JjDLURI3nQopmLLgo9ANKcJLO5FwT4W99bafV2ju-bbjRFVUQks0a856iVxIChDg8BWc5AyTtoRtqvZh7nUDLGpmJlWF3Xaw4tQZjeot3LBd4v7CLvF25rrxOatDg0S0mbQEUVjqSXn0nAAgQZmUN42FHs15_OME_wRHp-pI9aiIduvDYm9WBV05xpkmyRX0MTURJCHrvnOM3l7vjoyxXI13_NmsRpAl1-LANNXkzVUUtSi6gLhS6Mf6waX3_wwZ__952Gj4pVTGvHdRinf0',
-    expiryTime: '3600'
+    expiryTime: '3600',
   },
   actions,
   mutations,
