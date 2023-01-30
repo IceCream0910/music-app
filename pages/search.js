2c0249f47c0b32a1cf17a175dfe45487abee5682
Icecream0910 <taein2370@naver.com>
2023-01-30 18:11:55 +0900

    ROLLBACK: unuse trackItem component

--- pages/search.js
+++ pages/search.js
@@ -110,7 +110,24 @@ const SearchPage = (props) => {
           <div>
             <div className="result-container">
               {trackData.map((item) => (
-                <TrackItem item={item} key={item.id} />
+                  <div
+                      className="item track"
+                      key={item.id}>
+                    <div className="imageContainer">
+                      <img className="foregroundImg" src={item.image} />
+                      <img className="backgroundImg" src={item.image} />
+                    </div>
+                    <div className="left">
+                      <div style={{ fontWeight: 'bold', fontSize: '18px' }}>{item.title}</div>
+                      <div style={{ fontWeight: 'light', fontSize: '14px', opacity: 0.6 }}>{item.artist}—{item.album}</div>
+                    </div>
+                    <div className="right">
+                      <div className="clickable" onClick={() => handleItemClick(item.id)}><IonIcon name="play" /></div>
+                    </div>
+                  </div>
+              ))}
+            </div>
+          </div>
               ))}
             </div>
           </div>
